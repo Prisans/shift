f4054a90e1bff4c939f4c1db15d5d200231444b6
Prisans <124236508+Prisans@users.noreply.github.com>
2024-09-21 20:42:31 +0530

    Footer completed

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,27 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer">
+
+      <div className="footer-container">
+
+        <div className="footer-container-top">
+          <p>BASED IN TOKYO</p>
+          <p>WORKING WORLDWIDE</p>
+        </div>
+
+        <div className="footer-container-middle">
+          <h1>GET <span>IN</span> TOUCH</h1>
+          <button>CONTACT</button>
+        </div>
+
+        <div className="footer-container-bottom">
+          <p>Copyright @2024</p>
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
